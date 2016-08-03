d7d70511657326e4d370c4ae74889986ce88a612
Joppe Geluykens <joppe@youngwolves.co>
2016-08-03 22:49:43 +0200

    Remove useless test

--- VierOpEenRijTests/UnitTest1.vb
+++ VierOpEenRijTests/UnitTest1.vb
@@ -5,11 +5,6 @@ Imports frmMain = VierOpEenRij.frmMain
 
 <TestClass()> Public Class UnitTest1
 
-    <TestMethod()> Public Sub TestSize500300()
-        Dim frm = New frmMain
-        Assert.AreEqual(frm.Size, New Size(500, 300))
-    End Sub
-
     <TestMethod()> Public Sub TestRightWidthHeight()
         ' Assert.Equals(frmMain.BOARD_WIDTH, 
     End Sub
