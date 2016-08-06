a8908df52eac28564d0bf6d64799b692b9063265
Joppe Geluykens <joppe@youngwolves.co>
2016-08-06 19:41:07 +0200

    Game UX
    Add Btn class + add buttons to game panel
    Add common handler for all game panel controls
    Add Player class
    Add Start method to game
    Implement dropping of coin
    Add documentation

--- VierOpEenRij/Game.vb
+++ VierOpEenRij/Game.vb
@@ -1,27 +1,46 @@
 ﻿Option Explicit On
 Option Strict On
 
+
+Friend Class Player
+
+    Property color As Color
+    Property name As String
+    Property id As Integer
+
+
+    Sub New(ByVal id As Integer, ByVal color As Color)
+        Me.id = id
+        Me.color = color
+    End Sub
+End Class
+
+
 Friend Class Game
 
-    Enum Player
-        NONE
-        One
-        Two
-    End Enum
+    Property players As Queue(Of Player) = New Queue(Of Player)
+    Property active_player As Player
 
-    Public Shared Function GetColor(ByVal player As Player) As Color
-        Select Case player
-            Case Player.One
-                Return Color.Gold
-            Case Player.Two
-                Return Color.OrangeRed
-            Case Player.NONE
-                Return Color.Gray
-            Case Else
-                Return Color.Gray
-        End Select
-    End Function
 
-    Public Sub New()
+    Sub New()
+        players.Enqueue(New Player(1, Color.Gold))
+        players.Enqueue(New Player(2, Color.OrangeRed))
+    End Sub
+
+    Friend Sub Start(ByVal names As String())
+        ' Add all given names to the according players.
+        For i As Integer = 0 To names.GetUpperBound(0)
+            players(i).name = names(i)
+        Next
+        ' Make first player the active player.
+        active_player = players.Dequeue
+        players.Enqueue(active_player)
+        ' Enable clicking on coins and buttons!
+        frmMain.pnl_game.Enabled = True
+    End Sub
+
+    Friend Sub SwitchActivePlayer()
+        active_player = players.Dequeue
+        players.Enqueue(active_player)
     End Sub
 End Class
--- VierOpEenRij/frmMain.vb
+++ VierOpEenRij/frmMain.vb
@@ -1,16 +1,32 @@
 ﻿Option Explicit On
 Option Strict On
 
+
 Public Class frmMain
 
-    Dim Game As Game = New Game()
+    Dim game As Game = New Game()
 
-    Public Const BOARD_WIDTH As Integer = 7
-    Public Const BOARD_HEIGHT As Integer = 6
+    Const BOARD_WIDTH As Integer = 7
+    Const BOARD_HEIGHT As Integer = 6
+
 
     Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Dim board = New Board(BOARD_WIDTH - 1, BOARD_HEIGHT - 1)
+        Dim board = New Board(BOARD_WIDTH - 1, BOARD_HEIGHT - 1, game)
         board.AddToPanel(pnl_game)
     End Sub
 
+    Private Sub btn_start_Click(sender As Object, e As EventArgs) Handles btn_start.Click
+        DisableControl(CType(btn_start, Control))
+        DisableControl(CType(txt_player1, Control))
+        DisableControl(CType(txt_player2, Control))
+        game.Start(New String() {txt_player1.Text, txt_player2.Text})
+    End Sub
+
+    Private Sub DisableControl(ByRef control As Control)
+        control.Enabled = False
+    End Sub
+
+    Private Sub btn_stop_Click(sender As Object, e As EventArgs) Handles btn_stop.Click
+        Close()
+    End Sub
 End Class
